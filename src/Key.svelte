9cced303c5df0ff20319cdeab14c23270a89357d
BedWords <becca.sorestad@gmail.com>
2022-01-18 19:16:08 -0500

    fifth commit

--- src/Key.svelte
+++ src/Key.svelte
@@ -29,7 +29,7 @@
     afterUpdate(calculateColor);
 
     function clickHandler() {
-        alert("ARGHHHHHH");
+        alert("ehhhhhh?");
         dispatch('keyEvent', {key: letter});
     }
 </script>
