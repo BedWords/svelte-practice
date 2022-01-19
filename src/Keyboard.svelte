71982289af61d7148327658c1b10b963b0b17041
iandoesallthethings <denkyuu@gmail.com>
2022-01-18 19:36:38 -0500

    Fix keyboard alignment.

--- src/Keyboard.svelte
+++ src/Keyboard.svelte
@@ -1,62 +1,70 @@
 <script>
-    import Key from './Key.svelte';
-    import {createEventDispatcher} from 'svelte';
-    export let length;
+	import Key from './Key.svelte'
+	import { createEventDispatcher } from 'svelte'
+	export let length
 
-    export let guesses;
-    export let correctWord;
+	export let guesses
+	export let correctWord
 
-    const dispatch = createEventDispatcher();
-    let guess = "";
+	const dispatch = createEventDispatcher()
+	let guess = ''
 
-     let rows = [
-        ['q','w','e','r','t','y','u','i','o','p'],
-        ['a','s','d','f','g','h','j','k','l' ],
-        ['ENTER','z','x','c','v','b','n','m','<-']
-    ];
+	let rows = [
+		['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+		['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+		['ENTER', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<-']
+	]
 
-    function eventHandler(event){
-        let key = event.detail.key;
-        
-        if(key=='ENTER'){ submit(guess); }
-        else if(key=='<-'){ backspace(); }
-        else{ guess += key; }
-    }
+	function eventHandler(event) {
+		let key = event.detail.key
 
-    function submit(submission){
-        if (guess.length!=length){
-            alert('GUESS MUST BE ' + length + ' LETTERS LONG!');
-            guess = '';
-            return;
-        }
-        dispatch('submitEvent', {submission: guess});
-        guess = "";
-    }
+		if (key == 'ENTER') {
+			submit(guess)
+		} else if (key == '<-') {
+			backspace()
+		} else {
+			guess += key
+		}
+	}
 
-    function backspace(){
-       guess = guess.slice(0,guess.length-1);
-    }
+	function submit(submission) {
+		if (guess.length != length) {
+			alert('GUESS MUST BE ' + length + ' LETTERS LONG!')
+			guess = ''
+			return
+		}
+		dispatch('submitEvent', { submission: guess })
+		guess = ''
+	}
+
+	function backspace() {
+		guess = guess.slice(0, guess.length - 1)
+	}
 </script>
 
-
 <div class="kb">
-WORD: {guess}
+	WORD: {guess}
 
-
-{#each rows as row}
-    <p>
-    {#each row as letter}
-        <Key {letter} {guesses} {correctWord} on:keyEvent={eventHandler}/>
-    {/each}
-    </p>
-{/each} 
+	{#each rows as row}
+		<p class="row">
+			{#each row as letter}
+				<Key {letter} {guesses} {correctWord} on:keyEvent={eventHandler} />
+			{/each}
+		</p>
+	{/each}
 </div>
 
 <style>
-    div.kb {
-        display: block;
-        max-width: 25%;
-        margin: auto;
-        align-items:center;
-    }
+	div.kb {
+		max-width: 25%;
+		margin: auto;
+		display: flex;
+		flex-direction: column;
+	}
+
+	p.row {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
 </style>
